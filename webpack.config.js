423ff3c4e858a712d8c4cf3e76a04bd4d55b7db2
Matthew Toledo <matthew.toledo@gmail.com>
2016-01-22 15:25:51 -0500

    3.4 Developing the First React View

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,33 @@ const common = {
 	// Entry accepts a path or an object of entries
 	// The build chapter contains an example of the latter.
 	entry: PATHS.app,
+	// Add resolve.extensions.
+	// '' is needed to allow imorts without an extension.
+	// Note the .'s before extensions as it will fail to match without !!!
+	resolve: {
+		extensions: ['', '.js', '.jsx']
+	},
 	output: {
 		path: PATHS.build,
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: PATHS.app
+			},
+			// Set up jsx. This accepts js too thanks to RegExp
+			{
+				test: /\.jsx?$/,
+				// Enable caching for improved performance during development
+				// It uses default OS directory by default. If you need something
+				// more custom, pass a path to it.  I.e. , babel?cacheDirectory=<path>
+				loaders: ['babel?cacheDirectory'],
+				include: PATHS.app
+			}
+		]
 	}
 };
 
@@ -39,18 +63,6 @@ if (TARGET === 'start' || !TARGET) {
 			// Parse host and port from env so this is easy to customize.
 			host: process.env.HOST,
 			port: process.env.PORT
-		},
-		module: {
-			loaders: [
-				{
-					// Test expects a RegExp! Note the slashes!
-					test: /\.css$/,
-					loaders: ['style','css'],
-
-					// Include accepts either a path or an array of paths.
-					include : PATHS.app
-				}
-			],
 		},
 		plugins: [
 			new webpack.HotModuleReplacementPlugin()
