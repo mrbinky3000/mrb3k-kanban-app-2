314409ee40e1bb13f5f0583d1e460e4cfc59f2ed
Matthew Toledo <matthew.toledo@gmail.com>
2016-01-22 16:08:13 -0500

    4.4 Adding New Items to the List

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,28 +2,65 @@ import uuid from 'node-uuid';
 import React from 'react';;
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn Webpack'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do Laundry'
+				}
+			]
+		}
+
+	}
+
 	render() {
-		const notes = [
-			{
-				id: uuid.v4(),
-				task: 'Learn Webpack'
-			},
-			{
-				id: uuid.v4(),
-				task: 'Learn React'
-			},
-			{
-				id: uuid.v4(),
-				task: 'Do Laundry'
-			}
-		];
+		const notes = this.state.notes;
 
 		return (
 			<div>
+				<button onClick={this.addNote}>+</button>
 				<ul>{notes.map(note =>
 					<li key={note.id}>{note.task}</li>
 				)}</ul>
 			</div>
 		);
-	}
+	};
+
+	// We are using an experimental feature know as property
+	// initializer here. It allows us to bind the method `this`
+	// to point at our *App* instacne.
+	//
+	// Alternatively we could `bind` at `constructor` using
+	// a line, such as this.addNote = this.addNote.bind(this);
+
+	addNote = () => {
+		// It would be possible to write this in an imperative style.
+		// I.E, through `this.state.notes.push` and then
+		// `this.setState({notes: this.state.notes})` to commit.
+		//
+		// I tend to facor functional style whenever that makes sense.
+		// Even though it might take more code sometimes, I feel
+		// the benefites (easy to readson about, no side effects)
+		// more than make up for it.
+		//
+		// Libraries, such as Immutable.js, go a notch further.
+		this.setState({
+			notes: this.state.notes.concat([{
+				id: uuid.v4(),
+				task: 'New task'
+			}])
+		});
+	};
+
 }
