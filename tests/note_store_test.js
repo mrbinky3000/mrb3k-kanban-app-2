15872f2023696387ff0152b751cc89bcd0ae1edf
mrbinky3000 <mrbinky3000@gmail.com>
2016-01-25 17:07:10 -0500

    9.4 Testing Kanban Stores

---
+++ tests/note_store_test.js
@@ -0,0 +1,80 @@
+import assert from 'assert';
+import NoteActions from 'app/actions/NoteActions';
+import NoteStore from 'app/stores/NoteStore';
+import alt from 'app/libs/alt';
+
+
+describe ('NoteStore', () => {
+
+	beforeEach(() => {
+		alt.flush();
+	});
+
+	/**
+	 * NoteStore Allows create
+	 */
+	it('creates notes', () => {
+		const task = 'test';
+
+		NoteActions.create({task});
+
+		const state = NoteStore.getState();
+
+		assert.equal(state.notes.length, 1);
+		assert.equal(state.notes[0].task, task);
+	});
+
+
+	/**
+	 * NoteStore Allows update
+	 */
+	it('updates notes', () => {
+		const task = 'test';
+		const updatedTask = 'test 2';
+
+		NoteActions.create({task});
+
+		const note = NoteStore.getState().notes[0];
+
+		NoteActions.update({...note, task:updatedTask});
+
+		const state = NoteStore.getState();
+
+		assert.equal(state.notes.length, 1);
+		assert.equal(state.notes[0].task, updatedTask);
+	});
+
+	/**
+	 * NoteStore Allows delete
+	 */
+	it('deletes notes', () => {
+		NoteActions.create({task: 'test'});
+
+		const note = NoteStore.getState().notes[0];
+
+		NoteActions.delete(note.id);
+
+		const state = NoteStore.getState();
+
+		assert.equal(state.notes.length, 0);
+	});
+
+	/**
+	 * NoteStore Allows get
+	 */
+	it('gets notes', () => {
+		const task = 'test';
+		NoteActions.create({task: task});
+
+		const note = NoteStore.getState().notes[0];
+
+		const notes = NoteStore.getNotesByIds([note.id]);
+
+		assert.equal(notes.length, 1);
+		assert.equal(notes[0].task, task);
+
+	});
+
+
+
+});
